894ddb6d428aba4e89a2bc1e4e62647a1846a0a0
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-11-09 17:30:13 +0100

    state created

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,46 @@
 import React from "react";
 
 export class App extends React.Component {
+  state = {
+    // global state
+    isLoading: false,
+    hasError: false,
+    errorMessage: "",
+    hasInfo: false,
+    infoMessage: "",
+    procedures: null,
+
+    // router state
+    notLoginUserRoute: "LOGIN", // "CREATE-ACCOUNT", "RECOVER-PASSWORD"
+    logedUserRoute: "WELCOME-PAGE", // "APP-PAGE", "DB-PAGE"
+
+    // user/auth state
+    isUserLoged: false,
+    userFirstName: "",
+    userEmail: "",
+    userAvatar: "",
+
+    // login state
+    loginEmail: "",
+    loginPassword: "",
+
+    // createAccount state
+    createEmail: "",
+    createPassword: "",
+    createRepeatePassword: "",
+
+    // recover sate
+    recoverPasswordEmail: "",
+
+    // counting app state
+    timeInput: "",
+    procedureInput: ["", "", "", "", ""],
+
+    // editDatabase state
+    searchPhrase: "",
+    outputData: ["", "", ""],
+  };
+
   render() {
     return <div>Hello world</div>;
   }
