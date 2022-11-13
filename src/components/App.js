c968b35f24cc56a9d336aaeded6bed49ade9b388
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-11-13 17:57:45 +0100

    `LoginForm` added

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import FullPageLoader from "./FullPageLoader/FullPageLoader";
 import FullPageMessage from "./FullPageMessage/FullPageMessage";
-import TextField from "./TextField";
+import FullPageLayout from "./FullPageLayout/FullPageLayout";
+import LoginForm from "./LoginForm";
 
 export class App extends React.Component {
   state = {
@@ -46,10 +47,39 @@ export class App extends React.Component {
   };
 
   render() {
-    const { isLoading, hasError, errorMessage, hasInfo, infoMessage } =
-      this.state;
+    const {
+      isLoading,
+      hasError,
+      errorMessage,
+      hasInfo,
+      infoMessage,
+      notLoginUserRoute,
+      loginEmail,
+      loginPassword,
+    } = this.state;
     return (
       <>
+        {notLoginUserRoute === "LOGIN" ? (
+          <FullPageLayout>
+            <LoginForm
+              email={loginEmail}
+              password={loginPassword}
+              onChangeEmail={(e) => {
+                const { value } = e.target;
+                this.setState(() => ({ loginEmail: value }));
+              }}
+              onChangePassword={(e) => {
+                const { value } = e.target;
+                this.setState(() => ({ loginPassword: value }));
+              }}
+              onClickLogin={() => console.log("onClickLogin")}
+              onClickCreateAccount={() => console.log("onClickCreateAccount")}
+              onClickRecoverPassword={() =>
+                console.log("onClickRecoverPassword")
+              }
+            />
+          </FullPageLayout>
+        ) : null}
         {isLoading ? <FullPageLoader /> : null}
         {hasInfo ? (
           <FullPageMessage
@@ -65,7 +95,6 @@ export class App extends React.Component {
             onButtonClick={() => console.log("error message")}
           />
         ) : null}
-        <TextField placeholder={"E-mail"} />
       </>
     );
   }
---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,83 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Logo from "./Logo";
+import TextField from "../TextField";
+import Typography from "../Typography";
+import Button from "../Button";
+
+import classes from "./styles.module.css";
+
+export const LoginForm = (props) => {
+  const {
+    className,
+    email,
+    password,
+    onChangeEmail,
+    onChangePassword,
+    onClickLogin,
+    onClickCreateAccount,
+    onClickRecoverPassword,
+    ...otherProps
+  } = props;
+  return (
+    <div
+      className={`${classes.root}${className ? ` ${className}` : ""}`}
+      {...otherProps}
+    >
+      <Logo className={classes.logo} />
+      <Typography className={classes.header} variant={"h4-extraLight"}>
+        Log In !
+      </Typography>
+      <TextField
+        className={classes.textField}
+        placeholder={"E-mail"}
+        value={email}
+        onChange={onChangeEmail}
+      />
+      <TextField
+        className={classes.textField}
+        placeholder={"Password"}
+        type={"password"}
+        value={password}
+        onChange={onChangePassword}
+      />
+      <Button
+        className={classes.button}
+        variant={"contained"}
+        color={"primary"}
+        onClick={onClickLogin}
+      >
+        LOGIN
+      </Button>
+      <Button
+        className={classes.button}
+        variant={"contained"}
+        color={"secondary"}
+        onClick={onClickCreateAccount}
+      >
+        CREATE ACCOUNT
+      </Button>
+      <Button
+        className={classes.button}
+        variant={"text"}
+        onClick={onClickRecoverPassword}
+      >
+        RECOVER PASSWORD
+      </Button>
+    </div>
+  );
+};
+
+LoginForm.propTypes = {
+  className: PropTypes.string,
+  email: PropTypes.string,
+  password: PropTypes.string,
+  onChangeEmail: PropTypes.func,
+  onChangePassword: PropTypes.func.isRequired,
+  onClickLogin: PropTypes.func.isRequired,
+  onClickCreateAccount: PropTypes.func.isRequired,
+  onClickRecoverPassword: PropTypes.func.isRequired,
+};
+
+export default LoginForm;
