da293a2b16d87a016e0927e5112646627f9ccdea
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-11-13 15:09:29 +0100

    `FullPageLayout` added

---
+++ src/components/FullPageLayout/FullPageLayout.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import classes from "./styles.module.css";
+
+export const FullPgeLayout = (props) => {
+  const { className, children, ...otherProps } = props;
+
+  return (
+    <div
+      className={`${classes.root}${className ? ` ${className}` : ""}`}
+      {...otherProps}
+    >
+      {children}
+    </div>
+  );
+};
+
+FullPgeLayout.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default FullPgeLayout;
