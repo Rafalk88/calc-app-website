da9af9a69102f3d1eabc77883967a916dde5abb8
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-12-18 16:28:43 +0100

    `Settings` component added

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './styles.module.css';
+
+export const Settings = (props) => {
+  const { className, ...otherProps } = props;
+  return (
+    <div
+      className={`${classes.root}${className ? ` ${className}` : ''}`}
+      {...otherProps}
+    >
+
+    </div>
+  );
+};
+
+Settings.propTypes ={
+  className: PropTypes.string,
+};
+
+export default Settings;
